a773ca30c44836e9d036045e6ec2d6e5133054d3
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-08-19 16:59:45 +0200

    Created Custom Login Page with Security

---
+++ src/main/java/com/ahmetsenocak/demosecurity/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.ahmetsenocak.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String getLoginPage(){
+        return "plain-login";
+    }
+}
