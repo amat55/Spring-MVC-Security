004e2ca77c75cd1f41ad97efb65d636eda41b015
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-08-22 16:44:28 +0200

    Created Restrict URLs Based on Roles

--- src/main/java/com/ahmetsenocak/demosecurity/controller/DemoController.java
+++ src/main/java/com/ahmetsenocak/demosecurity/controller/DemoController.java
@@ -10,4 +10,14 @@ public class DemoController {
     public String showPage() {
         return "home";
     }
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
 }
